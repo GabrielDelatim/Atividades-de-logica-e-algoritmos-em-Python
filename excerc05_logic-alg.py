7782ee9d836008866edab127c1be61e13632eb31
GabrielDelatim <gabrieldelatim@gmail.com>
2026-03-01 20:15:09 -0300

    Update excerc05_logic-alg.py

--- excerc05_logic-alg.py
+++ excerc05_logic-alg.py
@@ -1,5 +1,5 @@
 #raízes reais de uma equação do segundo grau
-
+import math
 #declaração de variáveis
 
 a: float = 0.0
@@ -14,10 +14,16 @@ raiz2: float = 0.0
 a = float(input("Digite o valor de a: "))
 b = float(input("Digite o valor de b: "))
 c = float(input("Digite o valor de c: "))
+
 delta = b**2 - 4*a*c
-raiz1 = -(b) - delta / a*2
-raiz2 = -(b) + delta / a*2
+
+if delta < 0:
+    print ("A equação não possui raízes reais")
+
+raiz1 = -(b) - (math.sqrt (delta)) / a*2
+raiz2 = -(b) + (math.sqrt (delta)) / a*2
 
 print ("As raizes da equação são" , raiz1 ,"e", raiz2 )
+
 
 #Fim
